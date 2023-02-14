4f1c521b7dee7008f73581c6fa054fcaf68cf8ee
Rune Finstad Halvorsen <rune@runefh.dev>
2023-02-14 01:24:23 +0100

    wip

--- process-tripletex.ts
+++ process-tripletex.ts
@@ -1,11 +1,10 @@
 import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
 
+/**
+ * This just runs pretty-printing on the file
+ */
+
 // The filename is the first invocation argument
 const filename = Deno.args[0]; // Same name as downloaded_filename
 const data = await readJSON(filename);
-
-// Pluck a specific key off
-// and write it out to a different file
-// Careful! any uncaught errors and the workflow will fail, committing nothing.
-const newfile = `subset_of_${filename}`;
-await writeJSON(newfile, data, null, 2);
+await writeJSON(filename, data, null, 2);
