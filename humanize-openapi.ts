c6f115b7ad289c19ccdb834ac5c2d5c6049cca55
Rune Finstad Halvorsen <rune@runefh.dev>
2023-02-16 02:57:18 +0100

    wip

--- humanize-openapi.ts
+++ humanize-openapi.ts
@@ -1,73 +1,112 @@
 import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
 import { orderBy } from "https://esm.sh/natural-orderby@3.0.2";
+import wordWrap from "https://esm.sh/word-wrap";
 
-const roughOpenApiSchema = z.object({
-  paths: z.record(
-    z.string(),
-    z.record(
-      z.string(),
-      z.object({
-        description: z.string(),
-        summary: z.string().optional(),
-        parameters: z.array(
-          z.object({
-            name: z.string(),
-            in: z.string(),
-            required: z.boolean().optional(),
-            schema: z.unknown(),
-          })
-        ),
-        responses: z.record(
-          z.string(),
-          z.object({
-            description: z.string(),
-            content: z.unknown(),
-            schema: z.unknown(),
-          })
-        ),
-      })
-    )
-  ),
-});
+const optionalString = z
+  .string()
+  .optional()
+  .transform((e) => (typeof e === "string" && e.trim() !== "" ? e : undefined));
 
-const methodOrder = ["get", "post", "put", "patch", "delete"];
-
-function prettyPrint(data: z.infer<typeof roughOpenApiSchema>) {
-  const items = Object.entries(data.paths).flatMap(([path, methodDefs]) => {
-    const methods = Object.entries(methodDefs).map(([method, definition]) => {
-      const params = orderBy(definition.parameters, [
-        (e) => {
-          switch (e.in) {
-            case "path":
-              return 1;
-            case "query":
-              return 2;
-            case "body":
-              return 3;
-            default:
-              return 4;
-          }
-        },
-      ]);
-      return {
-        method,
-        definition,
-        params,
-        description: definition.description || definition.summary,
-      };
-    });
-    return { path, methods: orderBy(methods, [(e) => e.method]) };
+const roughParameters = z
+  .array(
+    z.object({
+      name: z.string(),
+      in: z.string(),
+      required: z.boolean().optional(),
+      schema: z.unknown(),
+    })
+  )
+  .transform((e) => {
+    const byPath = e.filter((v) => v.in === "path");
+    const byOther = e.filter((v) => v.in !== "path");
+    const sortedOther = orderBy(byOther, [
+      (e) => {
+        switch (e.in) {
+          case "path":
+            return 1;
+          case "query":
+            return 2;
+          case "body":
+            return 3;
+          default:
+            return 4;
+        }
+      },
+      (e) => e.name,
+    ]);
+    return [...byPath, ...sortedOther];
   });
 
-  const sorted = orderBy(items, [(e) => e.path]);
+const roughResponses = z
+  .record(
+    z.string(),
+    z.object({
+      description: optionalString,
+      content: optionalString,
+      schema: z.unknown(),
+    })
+  )
+  .transform((e) =>
+    Object.entries(e).map(([k, v]) => ({ statusCode: k, ...v }))
+  );
 
+const roughMethods = z
+  .record(
+    z.string(),
+    z
+      .object({
+        description: optionalString,
+        summary: optionalString,
+        parameters: roughParameters,
+        responses: roughResponses,
+      })
+      .transform((e) => {
+        console.log("asdf", e);
+        const { description, summary, ...rest } = e;
+        return { ...rest, description: description ?? summary };
+      })
+  )
+  .transform((e) =>
+    Object.entries(e).map(([k, v]) => ({ method: k.toUpperCase(), ...v }))
+  );
+
+const roughPaths = z
+  .record(z.string(), roughMethods)
+  .transform((e) =>
+    Object.entries(e).map(([k, v]) => {
+      return { path: k, methods: v };
+    })
+  )
+  .transform((arr) => orderBy(arr, [(e) => e.path]));
+
+type x = z.infer<typeof roughResponses>;
+
+const roughOpenApiSchema = z.object({
+  paths: roughPaths,
+});
+
+function toLines(line: string | undefined) {
+  if (line == null || line.trim() === "") {
+    return [""];
+  }
+  return wordWrap(line.trim(), { width: 60, trim: true, indent: "" }).split(
+    "\n"
+  );
+}
+
+function prettyPrint(data: z.infer<typeof roughOpenApiSchema>) {
   const lines: string[] = [];
 
-  for (const item of sorted) {
+  for (const item of data.paths) {
     lines.push(item.path);
     for (const method of item.methods) {
-      lines.push(`    ${method.method.toUpperCase()} - ${method.description}`);
-      for (const param of method.params) {
+      const descriptionParts = toLines(method.description);
+
+      lines.push(`    ${method.method}:    ${descriptionParts[0]}`);
+      if (descriptionParts.length > 1) {
+        lines.push(...descriptionParts.slice(1).map((e) => `            ${e}`));
+      }
+      for (const param of method.parameters) {
         lines.push(`        ${param.in.padEnd(8, " ")} ${param.name}`);
       }
       lines.push("");
@@ -79,8 +118,9 @@ function prettyPrint(data: z.infer<typeof roughOpenApiSchema>) {
   // console.log(JSON.stringify(sorted, null, 2));
 }
 
-const raw = await Deno.readTextFile("./tripletex-prod.json");
+// const raw = await Deno.readTextFile("./tripletex-prod.json");
+const raw = await Deno.readTextFile("./fiken.json");
 const json = JSON.parse(raw);
 const parsed = roughOpenApiSchema.parse(json);
-
+// console.log(JSON.stringify(parsed, null, 2));
 prettyPrint(parsed);
