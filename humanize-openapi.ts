bab8086b9cb9f2fc15a47cfb5d2678215d7381e0
Rune Finstad Halvorsen <rune@runefh.dev>
2023-02-20 01:52:02 +0100

    wip

--- humanize-openapi.ts
+++ humanize-openapi.ts
@@ -1,10 +1,21 @@
-import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
+// import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts";
 import { orderBy } from "https://esm.sh/natural-orderby@3.0.2";
 import swaggerParser from "https://esm.sh/@apidevtools/swagger-parser@10.1.0";
 import converter from "https://esm.sh/swagger2openapi@7.0.8";
 import { OpenAPIV3 } from "https://esm.sh/openapi-types@12.1.0";
 import redent from "https://esm.sh/redent@4.0.0";
 import wordWrap from "https://esm.sh/word-wrap@1.2.3";
+
+export default function invariant(
+  condition: unknown,
+  message?: string
+): asserts condition {
+  if (condition) {
+    return;
+  } else {
+    throw new Error(message ?? "Invariant error");
+  }
+}
 
 export function notEmpty<T>(value: T | null | undefined): value is T {
   return value !== null && value !== undefined;
@@ -13,12 +24,17 @@ export function notEmpty<T>(value: T | null | undefined): value is T {
 type MyParam =
   | {
       kind: "param";
-      name: string;
       deprecated?: boolean;
-      required?: boolean;
+      description?: string;
+      name: string;
       pIn: string;
+      refName?: string;
+      required?: boolean;
     }
-  | { kind: "ref"; target: string };
+  | {
+      kind: "ref";
+      target: string;
+    };
 
 function parsePath(path: string, pathObj: OpenAPIV3.PathItemObject) {
   const commonParams = (pathObj.parameters ?? []).map<MyParam>((e) => {
@@ -68,24 +84,59 @@ function parsePath(path: string, pathObj: OpenAPIV3.PathItemObject) {
   return methods.flat();
 }
 
+function parseParameterRef(
+  name: string,
+  param: OpenAPIV3.ReferenceObject | OpenAPIV3.ParameterObject
+) {
+  if ("$ref" in param) {
+    throw new Error("Can this happen?");
+  }
+
+  const ret: MyParam = {
+    pIn: param.in,
+    kind: "param",
+    name: param.name,
+    deprecated: param.deprecated,
+    required: param.required,
+    refName: `#/components/parameters/${name}`,
+  };
+
+  return ret;
+}
+
 function mungApi(doc: OpenAPIV3.Document) {
   const paths = Object.entries(doc.paths)
     .map(([name, val]) => (val ? { name, val } : undefined))
     .filter(notEmpty)
     .flatMap((e) => parsePath(e.name, e.val));
 
-  return { paths };
+  const parameters = Object.entries(doc.components?.parameters ?? {})
+    .map(([name, val]) => (val ? { name, val } : undefined))
+    .filter(notEmpty)
+    .flatMap((e) => parseParameterRef(e.name, e.val));
+
+  return { paths, parameters };
 }
 
 type Munged = ReturnType<typeof mungApi>;
 
 function prettyPrint(munged: Munged) {
+  const getParamRef = (ref: string) => {
+    const item = munged.parameters.find((e) => e.refName === ref);
+    invariant(item, `param ref not found: ${ref}`);
+    return item;
+  };
+
   console.log("Operations");
   console.log("==========\n");
   const orderedPaths = orderBy(munged.paths, [(e) => e.path, (e) => e.method]);
   for (const path of orderedPaths) {
+    const resolvedParams = path.params.map((e) =>
+      e.kind === "param" ? e : getParamRef(e.target)
+    );
+
     console.log(path.method.toUpperCase(), path.path);
-    const orderedParams = orderBy(path.params, [
+    const orderedParams = orderBy(resolvedParams, [
       (e) => (e.kind === "param" ? e.pIn : "z"),
       (e) => (e.kind === "param" ? e.name : "z"),
     ]);
@@ -93,12 +144,20 @@ function prettyPrint(munged: Munged) {
     const desc = wordWrap(path.description ?? "No description", { width: 76 });
     console.log(redent(desc, 2));
 
-    console.log("  Params:");
+    console.log("\n  Params:");
     for (const param of orderedParams) {
-      if (param.kind === "ref") {
-        continue;
+      if (param.refName) {
+        console.log(
+          `    ${param.pIn.padEnd(6, " ")} ${param.name} from ${param.refName}`
+        );
+      } else {
+        console.log(`    ${param.pIn.padEnd(6, " ")} ${param.name}`);
+
+        const desc = wordWrap(param.description ?? "No description", {
+          width: 72,
+        });
+        console.log(redent(desc, 6));
       }
-      console.log(`    ${param.pIn.padEnd(6, " ")} ${param.name}`);
     }
     console.log("\n");
   }
@@ -115,8 +174,9 @@ async function grabOpenapi3() {
 async function main() {
   const raw = await Deno.readTextFile("./fiken.json");
   const json = JSON.parse(raw);
-  const lal = await swaggerParser.dereference(json);
-  prettyPrint(mungApi(lal as any));
+  // const lal = await swaggerParser.dereference(json);
+  const munged = mungApi(json);
+  prettyPrint(munged);
 }
 
 main();
